b0710749a47d8531d73b7cdbc0819ab223e16a88
bzw <workbzw@outlook.com>
2022-12-03 22:00:28 +0800

    [update] 公安网备

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     name: 'helloworld',//此处为网站首页显示名称
     logo: 'logo.png',
     favicons: ['favicon.ico'],
-    footer: 'Copyright © 2022 | Powered by <a href="https://github.com/workbzw" target="_blank" rel="noreferrer">workbzw@outlook.com</a>'
+    footer: 'Copyright © 2022 | Powered by <a href="https://github.com/workbzw" target="_blank" rel="noreferrer">workbzw@outlook.com</a>\t\t \t<div style="width:300px;margin:0 auto; padding:20px 0;"><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11011402013434" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="gawb.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京公网安备 11011402013434号</p></a>\n</div>'
   },
 });
--- docs/index.md
+++ docs/index.md
@@ -4,17 +4,6 @@ title: 'helloworld.today'
 description:  helloworld
 hero:
   title: 'Helloworld .today'
-  description:  # 配置页面简介，同时用于生成 <meta> 标签
-  actions:
-    - text: go
-      link: /go
-    - text: rust
-      link: /rust
-    - text: java
-      link: /java
-    - text: react
-      link: /react
-    - text: android
-      link: /android
+  description: 'Java Go Rust React Android 全栈技术文档' # 配置页面简介，同时用于生成 <meta> 标签
 ---
 
